982cd8ec9bd77622a28817c32099a2df22bbd3a4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-03-13 15:43:46 -0400

    Initial image download very slow

--- docker_engine_app/tests.py
+++ docker_engine_app/tests.py
@@ -1,5 +1,10 @@
 from django.test import TestCase
+import docker
 
 class DockerTests(TestCase):
-    def test_image_list(self):
-        self.assertEqual(42, 42)
+    def test_hello_world(self):
+        # This seems to work, but it takes forever to download the image the first time.
+        # Is there something I should do differently?
+        client = docker.from_env()
+        output = client.containers.run("ubuntu", "echo hello world")
+        self.assertEqual(output, 'hello world\n')
