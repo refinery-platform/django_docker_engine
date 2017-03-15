57f8fccbf6bb516fbd7813a7d1a855eed1f6c6d8
mccalluc <mccalluc@users.noreply.github.com>
2017-03-15 17:17:26 -0400

    Mccalluc/symlink inputs (#13)
    * split out development requirements
    
    * contain spec test passes locally
    
    * flake8
    
    * factor out assertion
    
    * More factoring
    
    * Make setup into an instance method

--- django_docker_engine/tests.py
+++ django_docker_engine/tests.py
@@ -4,20 +4,36 @@ import datetime
 import re
 import requests
 import django
-from docker_utils import DockerClient
+from docker_utils import DockerClient, DockerContainerSpec
 from shutil import rmtree
 from time import sleep
 
 
 class DockerTests(unittest.TestCase):
+    def setUp(self):
+        # mkdtemp is the obvious way to do this, but
+        # the resulting directory is not visible to Docker.
+        base = '/tmp/django-docker-tests'
+        try:
+            os.mkdir(base)
+        except BaseException:
+            pass  # May already exist
+        self.tmp = os.path.join(
+            base,
+            re.sub(r'\W', '_', str(datetime.datetime.now())))
+        os.mkdir(self.tmp)
+
+    def tearDown(self):
+        rmtree(self.tmp)
+
     def timestamp(self):
         return re.sub(r'\W', '_', str(datetime.datetime.now()))
 
     def one_file_server(self, container_name, html):
-        with open(os.path.join(DockerTests.tmp, 'index.html'), 'w') as file:
+        with open(os.path.join(self.tmp, 'index.html'), 'w') as file:
             file.write(html)
         volume_spec = {
-            DockerTests.tmp: {
+            self.tmp: {
                 'bind': '/usr/share/nginx/html',
                 'mode': 'ro'}}
         ports_spec = {'80/tcp': None}
@@ -29,22 +45,17 @@ class DockerTests(unittest.TestCase):
                    ports=ports_spec)
         return client.lookup_container_port(container_name)
 
-    # TODO: Plain setup should be fine
-    @classmethod
-    def setUpClass(cls):
-        # mkdtemp is the obvious way to do this, but
-        # the resulting directory is not visible to Docker.
-        base = '/tmp/django-docker-tests'
-        try:
-            os.mkdir(base)
-        except BaseException:
-            pass  # May already exist
-        cls.tmp = base + '/' + re.sub(r'\W', '_', str(datetime.datetime.now()))
-        os.mkdir(cls.tmp)
+    def assert_url_content(self, url, content, client=django.test.Client()):
+        for i in xrange(10):
+            response = client.get(url)
+            if response.status_code == 200:
+                self.assertEqual(response.content, content)
+                return
+            sleep(1)
+        self.fail('Never got 200')
 
-    @classmethod
-    def tearDownClass(cls):
-        rmtree(cls.tmp)
+    # Tests at the top are low level;
+    # Tests at the bottom are at higher levels of abstraction.
 
     def test_hello_world(self):
         input = 'hello world'
@@ -53,9 +64,9 @@ class DockerTests(unittest.TestCase):
 
     def test_volumes(self):
         input = 'hello world\n'
-        with open(os.path.join(DockerTests.tmp, 'world.txt'), 'w') as file:
+        with open(os.path.join(self.tmp, 'world.txt'), 'w') as file:
             file.write(input)
-        volume_spec = {DockerTests.tmp: {'bind': '/hello', 'mode': 'ro'}}
+        volume_spec = {self.tmp: {'bind': '/hello', 'mode': 'ro'}}
         output = DockerClient().run('alpine:3.4', 'cat /hello/world.txt',
                                     volumes=volume_spec)
         self.assertEqual(output, input)
@@ -64,29 +75,26 @@ class DockerTests(unittest.TestCase):
         container_name = self.timestamp()
         hello_html = '<html><body>hello direct</body></html>'
         port = self.one_file_server(container_name, hello_html)
-        for i in xrange(10):
-            r = requests.get('http://localhost:{}/'.format(port))
-            if r.status_code == 200:
-                self.assertEqual(r.text, hello_html)
-                return
-            sleep(1)
-        self.fail('Never got 200')
+        url = 'http://localhost:{}/'.format(port)
+        self.assert_url_content(url, hello_html, client=requests)
 
     def test_docker_proxy(self):
         container_name = self.timestamp()
         hello_html = '<html><body>hello proxy</body></html>'
         self.one_file_server(container_name, hello_html)
-        c = django.test.Client()
-        for i in xrange(10):
-            r = c.get('/docker/{}/'.format(container_name))
-            if r.status_code == 200:
-                self.assertEqual(r.content, hello_html)
-                return
-            sleep(1)
-        self.fail('Never got 200')
+        url = '/docker/{}/'.format(container_name)
+        self.assert_url_content(url, hello_html)
 
-    # def test_higlass_proxy(self):
-    #     c = django.test.Client()
-    #     r = c.get('/docker/proxy_any_host/higlass.io/app/')
-    #     self.assertEqual(200, r.status_code)
-    #     self.assertRegexpMatches(r.content, r'HiGlass')
+    def test_container_spec(self):
+        input = 'hello world'
+        input_file = os.path.join(self.tmp, 'index.html')
+        with open(input_file, 'w') as file:
+            file.write(input)
+        container_name = self.timestamp()
+        DockerContainerSpec(
+            image_name='nginx:1.10.3-alpine',
+            container_name=container_name,
+            input_mount='/usr/share/nginx/html',
+            input_files=[input_file]).run()
+        url = '/docker/{}/'.format(container_name)
+        self.assert_url_content(url, input)
