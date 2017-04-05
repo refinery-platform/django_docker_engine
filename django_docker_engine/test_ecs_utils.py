7752571a106cd406a07f1fca4b2d0fdfbcc9a313
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-04-05 16:13:46 -0400

    Wait until task has started

--- django_docker_engine/test_ecs_utils.py
+++ django_docker_engine/test_ecs_utils.py
@@ -1,6 +1,8 @@
 import unittest
 import boto3
 import logging
+import time
+
 logging.basicConfig(level=logging.INFO)
 
 class EcsTests(unittest.TestCase):
@@ -9,18 +11,20 @@ class EcsTests(unittest.TestCase):
         self.ecs_client = boto3.client('ecs')
         self.ec2_client = boto3.client('ec2')
         self.key_pair_name = 'test_django_docker'
+        self.cluster_name = 'test_cluster'
         self.instance = None
 
     def tearDown(self):
         logging.info('tearDown')
         self.ec2_client.delete_key_pair(KeyName=self.key_pair_name)
         self.instance.terminate()
+        #self.ecs_client.delete_cluster(cluster=self.cluster_name)
+        # "The Cluster cannot be deleted while Container Instances are active or draining."
 
     def test_create_cluster(self):
         logging.info('create_cluster')
 
-        CLUSTER_NAME = 'test_cluster'
-        response = self.ecs_client.create_cluster(clusterName=CLUSTER_NAME)
+        response = self.ecs_client.create_cluster(clusterName=self.cluster_name)
         self.assertEqual(response['cluster']['status'], 'ACTIVE')
 
         logging.info('register_task_definition')
@@ -58,27 +62,43 @@ class EcsTests(unittest.TestCase):
             MinCount=1,
             MaxCount=1,
             InstanceType='t2.nano',
-            KeyName=self.key_pair_name
+            KeyName=self.key_pair_name,
+            UserData='\n'.join([
+                '#!/bin/bash',
+                'echo ECS_CLUSTER={} >> /etc/ecs/ecs.config'.format(self.cluster_name)]),
+            NetworkInterfaces=[
+                {
+                    'DeviceIndex': 0,
+                    'AssociatePublicIpAddress': True
+                }
+            ],
+            IamInstanceProfile={
+                # TODO: A new user will not already have this profile;
+                # TODO: Should we create it?
+                'Arn': 'arn:aws:iam::100836861126:instance-profile/ecsInstanceRole',
+                #'Name': 'optional?'
+            }
         )
         self.assertEqual(response['Instances'][0]['State']['Name'], 'pending')
         instance_id = response['Instances'][0]['InstanceId']
         self.instance = boto3.resource('ec2').Instance(instance_id)
-        self.instance.wait_until_running()
 
-        logging.info('TODO: register')
-        #
-        # # TODO: register instance with cluster
-        #
-        # response = self.ecs_client.run_task(
-        #     cluster=CLUSTER_NAME,
-        #     taskDefinition=TASK_NAME
-        # )
-        # print response
-        # # TODO: assert
-        #
-        # # Requires revision
-        # #response = self.ecs_client.deregister_task_definition()
-        #
-        #
-        # response = self.ecs_client.delete_cluster(cluster=CLUSTER_NAME)
-        # self.assertEqual(response['cluster']['status'], 'INACTIVE')
+        # We still had a race condition with this waiter:
+        # The instance may be up, but perhaps Docker engine?
+        # self.instance.wait_until_running()
+
+        response = None
+        while not response:
+            try:
+                response = self.ecs_client.run_task(
+                    cluster=self.cluster_name,
+                    taskDefinition=TASK_NAME
+                )
+            except Exception as e:
+                logging.info(e)
+                time.sleep(1)
+        logging.info(response)
+        # TODO: assert
+
+        # TODO: deregister_task requires revision
+        #response = self.ecs_client.deregister_task_definition()
