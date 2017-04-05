9c1ee98ace1746b601ba334d0e65adb7031dcd41
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-04-05 11:23:02 -0400

    Logging, so watching a test run is not so boring

--- django_docker_engine/test_ecs_utils.py
+++ django_docker_engine/test_ecs_utils.py
@@ -1,21 +1,29 @@
 import unittest
 import boto3
+import logging
+logging.basicConfig(level=logging.INFO)
 
 class EcsTests(unittest.TestCase):
     def setUp(self):
+        logging.info('setUp')
         self.ecs_client = boto3.client('ecs')
         self.ec2_client = boto3.client('ec2')
         self.key_pair_name = 'test_django_docker'
         self.instance = None
 
     def tearDown(self):
+        logging.info('tearDown')
         self.ec2_client.delete_key_pair(KeyName=self.key_pair_name)
         self.instance.terminate()
 
     def test_create_cluster(self):
+        logging.info('create_cluster')
+
         CLUSTER_NAME = 'test_cluster'
         response = self.ecs_client.create_cluster(clusterName=CLUSTER_NAME)
         self.assertEqual(response['cluster']['status'], 'ACTIVE')
+
+        logging.info('register_task_definition')
 
         TASK_NAME = 'test_task'
         response = self.ecs_client.register_task_definition(
@@ -37,8 +45,12 @@ class EcsTests(unittest.TestCase):
         )
         self.assertEqual(response['taskDefinition']['status'], 'ACTIVE')
 
+        logging.info('create_key_pair')
+
         response = self.ec2_client.create_key_pair(KeyName=self.key_pair_name)
         self.assertEqual(response['KeyName'], self.key_pair_name)
+
+        logging.info('run_instances')
 
         response = self.ec2_client.run_instances(
             ImageId='ami-275ffe31',
@@ -53,6 +65,7 @@ class EcsTests(unittest.TestCase):
         self.instance = boto3.resource('ec2').Instance(instance_id)
         self.instance.wait_until_running()
 
+        logging.info('TODO: register')
         #
         # # TODO: register instance with cluster
         #
