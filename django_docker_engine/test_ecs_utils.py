ca27116eebe001174b0fb53affd169b23c5a1463
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-04-04 20:05:27 -0400

    Checkpoint: start instance, register with ecs...

--- django_docker_engine/test_ecs_utils.py
+++ django_docker_engine/test_ecs_utils.py
@@ -3,16 +3,69 @@ import boto3
 
 class EcsTests(unittest.TestCase):
     def setUp(self):
-        self.client = boto3.client('ecs')
+        self.ecs_client = boto3.client('ecs')
+        self.ec2_client = boto3.client('ec2')
+        self.key_pair_name = 'test_django_docker'
+        self.instance_id = None
 
     def tearDown(self):
-        pass
+        self.ec2_client.delete_key_pair(KeyName=self.key_pair_name)
+        self.instance.terminate()
 
     def test_create_cluster(self):
-        NAME = 'foo'
-        response = self.client.create_cluster(clusterName=NAME)
+        CLUSTER_NAME = 'test_cluster'
+        response = self.ecs_client.create_cluster(clusterName=CLUSTER_NAME)
         self.assertEqual(response['cluster']['status'], 'ACTIVE')
-        #print response
-        response = self.client.delete_cluster(cluster=NAME)
-        self.assertEqual(response['cluster']['status'], 'INACTIVE')
-        #print response
+
+        TASK_NAME = 'test_task'
+        response = self.ecs_client.register_task_definition(
+            family=TASK_NAME,
+            containerDefinitions=[{
+                'name': 'my_container',
+                'image': 'nginx:1.11-alpine',
+                'portMappings': [
+                    {
+                        'containerPort': 80,
+                        'protocol': 'tcp'
+                        # host port will be assigned
+                    },
+                ],
+                # At least one required:
+                'memory': 100,  # Hard limit
+                'memoryReservation': 50, # Soft limit
+            }]
+        )
+        self.assertEqual(response['taskDefinition']['status'], 'ACTIVE')
+
+        response = self.ec2_client.create_key_pair(KeyName=self.key_pair_name)
+        self.assertEqual(response['KeyName'], self.key_pair_name)
+
+        response = self.ec2_client.run_instances(
+            ImageId='ami-275ffe31',
+            # us-east-1: amzn-ami-2016.09.g-amazon-ecs-optimized
+            MinCount=1,
+            MaxCount=1,
+            InstanceType='t2.nano',
+            KeyName=self.key_pair_name
+        )
+        self.assertEqual(response['Instances'][0]['State']['Name'], 'pending')
+        instance_id = response['Instances'][0]['InstanceId']
+        self.instance = self.ec2_client.Instance(instance_id)
+        self.instance.wait_until_running()
+
+        #
+        # # TODO: register instance with cluster
+        #
+        # response = self.ecs_client.run_task(
+        #     cluster=CLUSTER_NAME,
+        #     taskDefinition=TASK_NAME
+        # )
+        # print response
+        # # TODO: assert
+        #
+        # # Requires revision
+        # #response = self.ecs_client.deregister_task_definition()
+        #
+        #
+        # response = self.ecs_client.delete_cluster(cluster=CLUSTER_NAME)
+        # self.assertEqual(response['cluster']['status'], 'INACTIVE')
