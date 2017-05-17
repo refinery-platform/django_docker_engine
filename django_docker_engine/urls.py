f909d165eab75596d858c6a0fd29d4768ed954cb
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-05-17 15:22:52 -0400

    tests pass locally: move towards cleaner export of urlpatterns

--- django_docker_engine/urls.py
+++ django_docker_engine/urls.py
@@ -1,14 +1,3 @@
-from url_utils import proxy_url
+from proxy import Proxy, FileLogger
 
-urlpatterns = [
-    proxy_url()
-
-    # During development, it's useful to be able to test proxying,
-    # without also needing to start a container.
-    # url(r'^proxy_any_host/(?P<host>[^/]*)/(?P<url>.*)$',
-    #     lambda request, host, url:
-    #         HttpProxy.as_view(
-    #             base_url='http://{}/'.format(host)
-    #         )(request, url=url)
-    # ),
-]
+urlpatterns = Proxy(FileLogger('/tmp/django_docker_engine.log')).url_patterns()
--- django_docker_engine_demo/urls.py
+++ django_docker_engine_demo/urls.py
@@ -1,3 +1,6 @@
 from django.conf.urls import include, url
+from django_docker_engine.proxy import Proxy
 
-urlpatterns = [url(r'^docker/', include('django_docker_engine.urls'))]
+django_docker_engine_urls = include('django_docker_engine.urls')
+
+urlpatterns = [url(r'^docker/', django_docker_engine_urls)]
