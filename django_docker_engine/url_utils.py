461590214df4df61db271e5994e52893ad3e5766
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-05-17 11:53:01 -0400

    Requests are logged; TODO: configure file, or non-file dest.

--- django_docker_engine/url_utils.py
+++ django_docker_engine/url_utils.py
@@ -1,6 +1,9 @@
+from __future__ import print_function
+
 from django.conf.urls import url
 from httpproxy.views import HttpProxy
 from docker_utils import DockerClientWrapper
+from datetime import datetime
 
 def proxy_url():
     return url(
@@ -8,7 +11,13 @@ def proxy_url():
         _proxy_view
     )
 
+def _log(container_name, url):
+    with open('/tmp/refinery.txt', 'a') as f:
+        timestamp = datetime.now().isoformat()
+        print('\t'.join([timestamp, container_name, url]), file=f)
+
 def _proxy_view(request, container_name, url):
+    _log(container_name, url)
     container_url = DockerClientWrapper().lookup_container_url(container_name)
     view = HttpProxy.as_view(base_url=container_url)
     return view(request, url=url)
