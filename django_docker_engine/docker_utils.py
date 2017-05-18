6eb13feb9c5d5deecc4cfc73dd6ce935b7999e16
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-05-18 12:37:49 -0400

    Rename, and move the smaller class up top

--- django_docker_engine/docker_utils.py
+++ django_docker_engine/docker_utils.py
@@ -5,6 +5,20 @@ from time import time
 from container_managers import docker_engine
 
 
+
+class DockerContainerSpec():
+
+    def __init__(self, image_name, container_name,
+                 input={},
+                 container_input_path='/tmp/input.json',
+                 labels={}):
+        self.image_name = image_name
+        self.container_name = container_name
+        self.container_input_path = container_input_path
+        self.input = input
+        self.labels = labels
+
+
 class DockerClientWrapper():
 
     def __init__(self,
@@ -22,12 +36,16 @@ class DockerClientWrapper():
         return host_input_path
 
     def run(self, container_spec):
+        """
+        Run a given ContainerSpec. Returns the url for the container,
+        in contrast to the underlying method, which returns the logs.
+        """
         host_input_path = self._write_input_to_host(container_spec.input)
         volume_spec = [{
             'host': host_input_path,
             'bind': container_spec.container_input_path}]
         ports_spec = {'80/tcp': None}
-        self._sdk_run(container_spec.image_name,
+        self._manager_run(container_spec.image_name,
                       name=container_spec.container_name, # TODO: check
                       detach=True,
                       volumes=volume_spec,
@@ -35,16 +53,16 @@ class DockerClientWrapper():
                       labels=container_spec.labels)
         return self.lookup_container_url(container_spec.container_name)
 
-    def _sdk_run(self, image_name, cmd=None, volumes=(), **kwargs):
+    def _manager_run(self, image_name, cmd=None, volumes=(), **kwargs):
         """
-        Lower level wrapper for the SDK's run() method.
-        Significantly, "volumes" has a different structure here
+        Note that "volumes" has a different structure here
         than when using the SDK directly.
         """
         if (':' not in image_name):
             image_name += ':latest'
             # Without tag the SDK pulls every version; not what I expected.
             # https://github.com/docker/docker-py/issues/1510
+
         labels = kwargs.get('labels') or {}
         labels.update({self.root_label: 'true'})
         kwargs['labels'] = labels
@@ -53,7 +71,7 @@ class DockerClientWrapper():
         for volume in volumes:
             binding = volume.copy()
             if binding.get('mode'):
-                raise RuntimeError('"mode" should not be provided')
+                raise RuntimeError('"mode" should not be provided on {}'.format(volume))
             host_directory = binding.pop('host', None)
             if host_directory:
                 binding['mode'] = 'ro'  # For now, this is true.
@@ -62,6 +80,7 @@ class DockerClientWrapper():
                 host_directory = self._containers_manager.mkdtemp()
             volumes_dict[host_directory] = binding
         kwargs['volumes'] = volumes_dict
+        # Underlying method returns Docker logs
         return self._containers_manager.run(image_name, cmd, **kwargs)
 
     def lookup_container_url(self, container_name):
@@ -103,16 +122,3 @@ class DockerClientWrapper():
             # Doesn't work with non-integer values:
             # https://github.com/docker/docker-py/issues/1515
             return recent_log != ''
-
-
-class DockerContainerSpec():
-
-    def __init__(self, image_name, container_name,
-                 input={},
-                 container_input_path='/tmp/input.json',
-                 labels={}):
-        self.image_name = image_name
-        self.container_name = container_name
-        self.container_input_path = container_input_path
-        self.input = input
-        self.labels = labels
