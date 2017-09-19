d6444baac29d63db00c6e6527a1d4e138ef5fe03
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-09-19 19:27:35 -0400

    flake8

--- django_docker_engine/proxy.py
+++ django_docker_engine/proxy.py
@@ -62,7 +62,8 @@ class Proxy():
     def _proxy_view(self, request, container_name, url):
         self.logger.log(container_name, url)
         try:
-            container_url = DockerClientWrapper(self.data_dir).lookup_container_url(container_name)
+            client = DockerClientWrapper(self.data_dir)
+            container_url = client.lookup_container_url(container_name)
             view = HttpProxy.as_view(base_url=container_url)
             return view(request, url=url)
         except (NotFound, BadStatusLine) as e:
