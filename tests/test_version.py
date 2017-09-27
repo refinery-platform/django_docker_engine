a61e527fe33e6f3f9b9564db21955c9f5053a5e1
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-09-27 14:45:00 -0400

    VERSION in own file. Test increment

---
+++ tests/test_version.py
@@ -0,0 +1,23 @@
+import unittest
+import requests
+import os
+import re
+
+
+class VersionTests(unittest.TestCase):
+
+    def test_version_has_been_incremented(self):
+        version_re = r'^\d+\.\d+\.\d+$'
+        r = requests.get('https://pypi.python.org/pypi/django-docker-engine/json')
+        pypi_versions = r.json()['releases'].keys()
+
+        for v in pypi_versions:
+            self.assertRegexpMatches(v, version_re)
+
+        version_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), 'VERSION.txt')
+        local_version = open(version_path).read().strip()
+
+        self.assertRegexpMatches(local_version, version_re)
+
+        self.assertNotIn(local_version, pypi_versions)
+
