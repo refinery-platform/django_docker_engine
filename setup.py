8b27d5c0c8948e06cfb2b1f4c000ffb3e950542f
mccalluc <mccalluc@users.noreply.github.com>
2017-09-28 16:03:06 -0400

    During install, refinery couldn't find 'VERSION.txt' (#88)
    * During install, refinery couldn't find 'VERSION.txt'... I thought the chdir handled that?
    (Would also be nice to test the whole process on this side.)
    
    * bump
    
    * flake8

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
 import os
+from os.path import (join, normpath, dirname, abspath)
 from setuptools import find_packages, setup
 
-with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
+with open(join(dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
 
 # allow setup.py to be run from any path
-os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+os.chdir(normpath(join(abspath(__file__), os.pardir)))
 
 setup(
     name='django_docker_engine',
-    version=open('VERSION.txt').read().strip(),
+    version=open(join(dirname(__file__), 'VERSION.txt')).read().strip(),
     install_requires=[
         'django',
         'docker==2.1.0',
