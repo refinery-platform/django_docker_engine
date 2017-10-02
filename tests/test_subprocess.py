9365ca895be169afa4122cbe54e119c732eaf19c
mccalluc <mccalluc@users.noreply.github.com>
2017-10-02 16:15:29 -0400

    Mccalluc/set port in spec (#90)
    * Allow ports other than 80
    
    * end-to-end test in python
    
    * Update docs
    
    * flake8
    
    * Demonstrate from the commandline how to connect
    
    * Cleanup after myself

---
+++ tests/test_subprocess.py
@@ -0,0 +1,53 @@
+import unittest
+import requests
+import subprocess
+import time
+import socket
+from django_docker_engine.docker_utils import (DockerContainerSpec, DockerClientWrapper)
+from shutil import rmtree
+from os import mkdir
+
+
+class SubprocessTests(unittest.TestCase):
+    """
+    Check that the basic functionality works from end-to-end,
+    starting the django server as you would from the command-line.
+    """
+
+    def free_port(self):
+        s = socket.socket()
+        s.bind(('', 0))
+        return str(s.getsockname()[1])
+
+    def setUp(self):
+        self.port = self.free_port()
+        subprocess.Popen(['./manage.py', 'runserver', self.port])
+        time.sleep(1)
+        self.container_name = 'test-' + self.port
+        self.url = 'http://127.0.0.1:{}/docker/{}/'.format(self.port, self.container_name)
+        self.tmp_dir = '/tmp/test-' + self.port
+        mkdir(self.tmp_dir)
+        # TODO: Might use mkdtemp, but Docker couldn't see the directory?
+        # self.tmp_dir = mkdtemp()
+        # chmod(self.tmp_dir, 0777)
+
+    def tearDown(self):
+        rmtree(self.tmp_dir)
+
+    def test_please_wait(self):
+        r = requests.get(self.url)
+        self.assertIn('Please wait', r.content)
+
+    def test_container(self):
+        client = DockerClientWrapper(self.tmp_dir)
+        client.run(
+            DockerContainerSpec(
+                image_name='nginx:1.10.3-alpine',
+                container_name=self.container_name
+            )
+        )
+        time.sleep(1)
+        r = requests.get(self.url)
+        self.assertIn('nginx', r.content)
+
+        client.purge_by_label(self.container_name)
