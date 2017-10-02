4d8b9f40474b90e2bfe6b321d47cc56751efe2ba
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-02 16:11:18 -0400

    Cleanup after myself

--- tests/test_subprocess.py
+++ tests/test_subprocess.py
@@ -4,9 +4,15 @@ import subprocess
 import time
 import socket
 from django_docker_engine.docker_utils import (DockerContainerSpec, DockerClientWrapper)
+from shutil import rmtree
+from os import mkdir
 
 
 class SubprocessTests(unittest.TestCase):
+    """
+    Check that the basic functionality works from end-to-end,
+    starting the django server as you would from the command-line.
+    """
 
     def free_port(self):
         s = socket.socket()
@@ -19,18 +25,22 @@ class SubprocessTests(unittest.TestCase):
         time.sleep(1)
         self.container_name = 'test-' + self.port
         self.url = 'http://127.0.0.1:{}/docker/{}/'.format(self.port, self.container_name)
+        self.tmp_dir = '/tmp/test-' + self.port
+        mkdir(self.tmp_dir)
+        # TODO: Might use mkdtemp, but Docker couldn't see the directory?
+        # self.tmp_dir = mkdtemp()
+        # chmod(self.tmp_dir, 0777)
 
     def tearDown(self):
-        pass
-        # TODO: remove old container
-        # TODO: remove old tmp dir
+        rmtree(self.tmp_dir)
 
     def test_please_wait(self):
         r = requests.get(self.url)
         self.assertIn('Please wait', r.content)
 
     def test_container(self):
-        DockerClientWrapper('/tmp/test-' + self.port).run(
+        client = DockerClientWrapper(self.tmp_dir)
+        client.run(
             DockerContainerSpec(
                 image_name='nginx:1.10.3-alpine',
                 container_name=self.container_name
@@ -39,3 +49,5 @@ class SubprocessTests(unittest.TestCase):
         time.sleep(1)
         r = requests.get(self.url)
         self.assertIn('nginx', r.content)
+
+        client.purge_by_label(self.container_name)
