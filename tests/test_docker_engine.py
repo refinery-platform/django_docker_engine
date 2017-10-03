3adedfe4595ed5a2462e9280d997e5d0e9a0d8f4
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-03 15:24:01 -0400

    Make tests into asserts and we have less boilerplate

--- tests/test_docker_engine.py
+++ tests/test_docker_engine.py
@@ -13,42 +13,48 @@ class DockerEngineManagerTests(unittest.TestCase):
         self.root_label = 'test-root'
         self.manager = DockerEngineManager(data_dir, self.root_label)
         self.container_name = timestamp
-
-    def test_expected_errors(self):
-        kwargs = {
-            'name': self.container_name,
+        self.kwargs = {
             'cmd': None
         }
-        self.manager.run('alpine:3.6', **kwargs)
-        with self.assertRaises(NoPortLabel):
+
+    def assert_add_kwarg_still_fails(self, key, value, image, expected_error):
+        self.kwargs[key] = value
+        self.manager.run(image, **self.kwargs)
+        if expected_error:
+            with self.assertRaises(expected_error):
+                self.manager.get_url(self.container_name)
+        else:
             self.manager.get_url(self.container_name)
-
+        # TODO: Why the '/'?
         self.manager.list({'name': '/' + self.container_name})[0].remove(force=True)
 
-        kwargs['labels'] = {self.root_label+'.port': '12345'}
-        self.manager.run('alpine:3.6', **kwargs)
-        with self.assertRaises(NoPortsOpen):
-            self.manager.get_url(self.container_name)
+    def test_expected_errors(self):
+        self.assert_add_kwarg_still_fails(
+            'name', self.container_name,
+            'alpine:3.6',
+            NoPortLabel
+        )
 
-        self.manager.list({'name': '/' + self.container_name})[0].remove(force=True)
+        self.assert_add_kwarg_still_fails(
+            'labels', {self.root_label+'.port': '12345'},
+            'alpine:3.6',
+            NoPortsOpen
+        )
 
-        kwargs['detach'] = True
-        self.manager.run('nginx:1.10.3-alpine', **kwargs)
-        with self.assertRaises(ExpectedPortMissing):
-            self.manager.get_url(self.container_name)
+        self.assert_add_kwarg_still_fails(
+            'detach', True,
+            'nginx:1.10.3-alpine',
+            ExpectedPortMissing
+        )
 
-        self.manager.list({'name': '/' + self.container_name})[0].remove(force=True)
+        self.assert_add_kwarg_still_fails(
+            'labels', {self.root_label+'.port': '80'},
+            'nginx:1.10.3-alpine',
+            MisconfiguredPort
+        )
 
-        kwargs['labels'] = {self.root_label + '.port': '80'}
-        self.manager.run('nginx:1.10.3-alpine', **kwargs)
-        with self.assertRaises(MisconfiguredPort):
-            self.manager.get_url(self.container_name)
-
-        self.manager.list({'name': '/' + self.container_name})[0].remove(force=True)
-
-        kwargs['ports'] = {'80/tcp': None}
-        self.manager.run('nginx:1.10.3-alpine', **kwargs)
-        self.manager.get_url(self.container_name)
-        # Finally works!
-
-        self.manager.list({'name': '/' + self.container_name})[0].remove(force=True)
+        self.assert_add_kwarg_still_fails(
+            'ports', {'80/tcp': None},
+            'nginx:1.10.3-alpine',
+            None
+        )
