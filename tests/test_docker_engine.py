b244ef52213ad848f102862c51bbfb1d092da9ea
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-03 12:08:47 -0400

    flake8

--- tests/test_docker_engine.py
+++ tests/test_docker_engine.py
@@ -2,7 +2,8 @@ import unittest
 from datetime import datetime
 import logging
 import re
-from django_docker_engine.container_managers.docker_engine import (DockerEngineManager, NoPortsOpen)
+from django_docker_engine.container_managers.docker_engine \
+    import (DockerEngineManager, NoPortsOpen)
 
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -16,12 +17,11 @@ class DockerEngineManagerTests(unittest.TestCase):
         root_label = 'test-root'
         manager = DockerEngineManager(data_dir, root_label)
         container_name = timestamp
-        manager.run('alpine:3.6',
-                    name=container_name,
-                    cmd=None,
-                    labels={root_label+'.port': '12345'}
+        manager.run(
+            'alpine:3.6',
+            name=container_name,
+            cmd=None,
+            labels={root_label+'.port': '12345'}
         )
         with self.assertRaises(NoPortsOpen):
             manager.get_url(container_name)
-
-
