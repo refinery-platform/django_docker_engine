81671ed5513648ef9568ca001b018dbd7405830e
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2017-10-04 12:32:32 -0400

    flake8

--- tests/test_hostname_routing.py
+++ tests/test_hostname_routing.py
@@ -8,7 +8,10 @@ class HostnameRoutingTest(unittest.TestCase):
     def test_hostname_routing(self):
         name = 'foobar'
         path = '/barfoo'
-        request = RequestFactory().get(path , HTTP_HOST='{}.docker.localhost'.format(name))
+        request = RequestFactory().get(
+            path,
+            HTTP_HOST='{}.docker.localhost'.format(name)
+        )
         self.assertEqual(request.path, path)
         HostnameRoutingMiddleware().process_request(request)
         self.assertEqual(request.path, '/docker/{}{}'.format(name, path))
