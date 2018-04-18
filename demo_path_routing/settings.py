71da45ea866ac8f89171c85c0169125ac296b155
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-18 11:46:45 -0400

    generalize settings.py

--- demo_path_routing/settings.py
+++ demo_path_routing/settings.py
@@ -8,20 +8,14 @@ SECRET_KEY = 'not-so-secret'
 DEBUG = True
 TEMPLATE_DEBUG = True
 
-ALLOWED_HOSTS = []
-
 INSTALLED_APPS = (
     'django_docker_engine',
     'revproxy'
 )
 
-MIDDLEWARE_CLASSES = ()
+ROOT_URLCONF = __package__ + '.urls'
+WSGI_APPLICATION = __package__ + '.wsgi.application'
 
-ROOT_URLCONF = 'demo_path_routing.urls'
-WSGI_APPLICATION = 'demo_path_routing.wsgi.application'
-
-# These are only really needed for hostname-based routing,
-# but tests also use this settings.py, but this is untidy.
 DJANGO_DOCKER_HOST_SUFFIX = '.docker.localhost'
 DJANGO_DOCKER_PATH_PREFIX = '/docker/'
 
@@ -34,3 +28,27 @@ USE_L10N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
+
+if __package__ != 'demo_host_routing':
+    ALLOWED_HOSTS = []
+    MIDDLEWARE_CLASSES = ()
+else:
+    ALLOWED_HOSTS = [
+        # This needs to allow the root server, and any
+        # subdomains which are mapped to containers.
+        # Leaving it as '*' is not good for security!
+        # https://docs.djangoproject.com/en/1.11/ref/settings/#allowed-hosts
+        '*'
+    ]
+
+    from django import VERSION
+    if VERSION >= (2, 0):
+        MIDDLEWARE = (
+            'django_docker_engine.middleware.hostname_routing.'
+            'HostnameRoutingMiddlewareCallable',
+        )
+    else:
+        MIDDLEWARE_CLASSES = (
+            'django_docker_engine.middleware.hostname_routing.'
+            'HostnameRoutingMiddleware',
+        )
