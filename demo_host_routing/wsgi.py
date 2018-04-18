eb1c05f9dc6e7ba2e2ff5cdb894dfe5f43e84b11
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-18 11:14:52 -0400

    symlink wsgi

--- demo_host_routing/wsgi.py
+++ demo_host_routing/wsgi.py
@@ -1,9 +1,1 @@
-import os
-
-import django.core.handlers.wsgi
-
-# Locally, I don't need this, but Travis fails without a wsgi.py in place.
-# Not sure why there's a difference.
-
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", __package__ + ".settings")
-application = django.core.handlers.wsgi.WSGIHandler()
+../demo_path_routing/wsgi.py
