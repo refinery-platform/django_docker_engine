273326cd69965a8584d2ce2693b5c9d180eabdb9
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-18 13:00:20 -0400

    Hello world

--- demo_path_routing/urls.py
+++ demo_path_routing/urls.py
@@ -1,4 +1,9 @@
 from django.conf.urls import include, url
 
+from . import views
+
+
 urlpatterns = [
-    url(r'^docker/', include(__package__ + '.proxy_url_patterns'))]
+    url(r'^$', views.index),
+    url(r'^docker/', include(__package__ + '.proxy_url_patterns'))
+]
