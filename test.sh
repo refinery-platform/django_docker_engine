2eb9c3a5909a425ffbf919104117a739a8df96ca
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-04-24 17:28:06 -0400

    Work around for log truncation on Travis

--- test.sh
+++ test.sh
@@ -3,7 +3,9 @@ set -o errexit
 
 start() { echo travis_fold':'start:$1; echo $1; }
 end() { echo travis_fold':'end:$1; }
-die() { set +v; echo "$*" 1>&2 ; exit 1; }
+die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
+# Race condition truncates logs on Travis: "sleep" might help.
+# https://github.com/travis-ci/travis-ci/issues/6018
 
 ping -c1 container-name.docker.localhost > /dev/null  # Prereq for hostname-based dispatch.
 docker info | grep 'Operating System'  # Are we able to connect to Docker, and what OS is it?
