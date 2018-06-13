fe7ba86aa0b92d153bcbe2ce4ade47025af6cc0c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-06-13 13:56:21 -0400

    add "manage_auth.py"... This seems better than passing settings through some other channel.

--- demo_path_routing_auth/setup-db.sh
+++ demo_path_routing_auth/setup-db.sh
@@ -1,8 +1,9 @@
 #!/usr/bin/env bash
 set -o errexit
 
-./manage.py migrate
-./manage.py shell --command '
+cd `dirname "$0"`
+../manage_auth.py migrate
+../manage_auth.py shell --command '
 from django.contrib.auth.models import User
 if not User.objects.count():
     User.objects.create_user("fake-username", "fake@example.com", "fake-password")
