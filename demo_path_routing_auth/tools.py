eeb1bfd4bef083d8053d185aec933d4665854c1c
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-08-20 20:31:37 -0400

    Upgrade IGV, add MultiQC

--- demo_path_routing_auth/tools.py
+++ demo_path_routing_auth/tools.py
@@ -20,17 +20,16 @@ tools = {
         'default_files': ['fruit.txt', 'green.txt', 'sweet.txt', 'vegetable.txt'],
         'input': lambda urls, prefix: {'file_relationships': urls}
     },
-    # TODO: docker_igv_js only accepts INPUT_JSON_URL right now.
-    # 'igv-js': {
-    #     'image': 'gehlenborglab/docker_igv_js:v0.0.7',
-    #     'description': 'Our wrapper for IGV.js',
-    #     'default_parameters': [
-    #         {'name': 'Genome Build',
-    #          'value': 'hg19'}
-    #     ],
-    #     'default_files': ['3x3.csv'],
-    #     'input': lambda urls, prefix: {'file_relationships': urls}
-    # },
+    'igv-js': {
+        'image': 'gehlenborglab/docker_igv_js:v0.0.9',
+        'description': 'Our wrapper for IGV.js',
+        'default_parameters': [
+            {'name': 'Genome Build',
+             'value': 'hg19'}
+        ],
+        'default_files': ['3x3.csv'],
+        'input': lambda urls, prefix: {'file_relationships': urls}
+    },
     'higlass': {
         'image': 'scottx611x/refinery-higlass-docker:v0.3.2',
         'description': '1-D and 2-D genomic data browser',
@@ -50,12 +49,19 @@ tools = {
         'default_parameters': [],
         'default_files': ['3x3.csv'],
         'input': lambda urls, prefix: {
-            'file_relationships': [
-                urls,  # Count data
-                []  # Differential expression data
-            ],
+            'file_relationships': [urls, []],
             'api_prefix': prefix,
             'extra_directories': ['/refinery-data/']
+        }
+    },
+    'multiqc': {
+        'image': 'mccalluc/qualimap_multiqc_refinery:v0.0.7',
+        'description': 'Wrapper for MultiQC',
+        'default_parameters': [],
+        'default_files': ['raw_data_qualimapReport.zip'],
+        'input': lambda urls, prefix: {
+            'file_relationships': [urls, []],
+            'extra_directories': ['/data/']
         }
     }
 }
--- demo_path_routing_auth/views.py
+++ demo_path_routing_auth/views.py
@@ -28,7 +28,9 @@ UPLOAD_DIR = os.path.join(os.path.dirname(__file__), 'upload')
 
 OUTSIDE_URLS = [
     'https://s3.amazonaws.com/pkerp/public/gene_annotations.short.db',
-    'https://s3.amazonaws.com/pkerp/public/cnv_short.hibed'
+    'https://s3.amazonaws.com/pkerp/public/cnv_short.hibed',
+    'https://beta.stemcellcommons.org/media/file_store/22/e3/'
+        'raw_data_qualimapReport.zip'
 ]
 
 
