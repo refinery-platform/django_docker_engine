7330b00976e88dd9f6d5b16d7b8aea73b742829a
Chuck McCallum <chuck_mccallum@hms.harvard.edu>
2018-09-18 15:35:07 -0400

    Bring in a couple pre-existing checks, and format.

--- test.sh
+++ test.sh
@@ -7,12 +7,15 @@ die() { set +v; echo "$*" 1>&2 ; sleep 1; exit 1; }
 # Race condition truncates logs on Travis: "sleep" might help.
 # https://github.com/travis-ci/travis-ci/issues/6018
 
-ping -c1 container-name.docker.localhost > /dev/null  # Prereq for hostname-based dispatch.
-docker info | grep 'Operating System'  # Are we able to connect to Docker, and what OS is it?
-
 start preflight
-[ -z "`docker ps -qa`" ] || die 'Kill containers before running tests: "docker ps -qa | xargs docker stop | xargs docker rm"'
-[ -z "`lsof -t -i tcp:8000`" ] || die 'Free port 80 before running tests: "lsof -t -i tcp:8000 | xargs kill"'
+ping -c1 container-name.docker.localhost > /dev/null \
+    || die 'Update /etc/hosts: "echo 127.0.0.1 container-name.docker.localhost >> /etc/hosts"'
+docker info | grep 'Operating System' \
+    || die 'Make sure Docker is running'
+[ -z "`docker ps -qa`" ] \
+    || die 'Kill containers before running tests: "docker ps -qa | xargs docker stop | xargs docker rm"'
+[ -z "`lsof -t -i tcp:8000`" ] \
+    || die 'Free port 80 before running tests: "lsof -t -i tcp:8000 | xargs kill"'
 end preflight
 
 start test
